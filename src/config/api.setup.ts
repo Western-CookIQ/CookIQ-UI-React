d9e1118132cc6ce5f6281232d847087fe48b6652
Keegan Mongru <kmongru78@gmail.com>
2024-01-10 23:21:07 -0500

    fix: add === to backend url parsing logic

--- src/config/api.setup.ts
+++ src/config/api.setup.ts
@@ -1,6 +1,10 @@
-let parts = window.location.href.split(":");
-let url = "";
-if ((parts[1] = "//localhost")) url = parts[0] + ":" + parts[1] + ":8080";
-else url = process.env.NODE_SERVER_URL || "";
+const parts = window.location.href.split(":");
+let url: string;
+
+if (parts[1] === "//localhost") {
+  url = `${parts[0]}:${parts[1]}:8080`;
+} else {
+  url = process.env.NODE_SERVER_URL || "";
+}
 
 export default url;
